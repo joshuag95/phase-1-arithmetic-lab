3fbc2b2c0c79ce972c56af084a277d0626982b02
Joshua Greer <jtgreer95@gmail.com>
2022-06-28 10:07:43 -0500

    commit tree d26e1cecbbb03b1c841543dc23d7e22b89230bd1

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 9
+const num4 = 5
+const mod = num3 % num4
+console.log(mod);
+
+
+const max = Math.max(1,5,8,10,20)
+console.log(max)
